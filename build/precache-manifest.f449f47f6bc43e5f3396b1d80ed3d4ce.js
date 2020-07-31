c6a1d0e336a0618af9d3a5b3e4004d8fc8589e28
ale_917k <alessiopetrin9@gmail.com>
2020-07-31 08:14:44 +0100

    Deployed on gh-pages

---
+++ build/precache-manifest.f449f47f6bc43e5f3396b1d80ed3d4ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95485d7d17fe2e5bd1d3f65052a50b7",
+    "url": "/ale917k/robofriend-redux/index.html"
+  },
+  {
+    "revision": "a21b992b13d6b50dca34",
+    "url": "/ale917k/robofriend-redux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "554ce5a6b01364642895",
+    "url": "/ale917k/robofriend-redux/static/css/main.f1eced3b.chunk.css"
+  },
+  {
+    "revision": "a21b992b13d6b50dca34",
+    "url": "/ale917k/robofriend-redux/static/js/2.bc05235e.chunk.js"
+  },
+  {
+    "revision": "bb8fb1c4ca51bfe3d3f76c8caf6f2c92",
+    "url": "/ale917k/robofriend-redux/static/js/2.bc05235e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554ce5a6b01364642895",
+    "url": "/ale917k/robofriend-redux/static/js/main.f7939d1d.chunk.js"
+  },
+  {
+    "revision": "9677609eb7857b9e8303",
+    "url": "/ale917k/robofriend-redux/static/js/runtime-main.b90bc682.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ale917k/robofriend-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
